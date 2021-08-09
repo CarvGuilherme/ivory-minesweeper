54a84024a8112e0909ff7ceab0a0f49c915e4446
Guilherme Augusto <71442931+CarvGuilherme@users.noreply.github.com>
2021-08-09 19:15:34 -0300

    Update Program.cs

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -58,9 +58,9 @@ namespace Ivory.TesteEstagio.CampoMinado
              
             
             // Método 3
-            class ArrayClass2D
+            class ArrayClass
          {
-        static void Print2DArray(int[,] arr)
+        static void PrintArray(int[,] arr)
         {
             // Display the array elements.
             for (int i = 0; i < arr.GetLength(0); i++)
@@ -74,23 +74,14 @@ namespace Ivory.TesteEstagio.CampoMinado
         static void Main()
         {
             // Pass the array as an argument.
-            Print2DArray(new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } });
+            PrintArray(new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } });
 
             // Keep the console window open in debug mode.
             System.Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
     }
-    /* Output:
-        Element(0,0)=1
-        Element(0,1)=2
-        Element(1,0)=3
-        Element(1,1)=4
-        Element(2,0)=5
-        Element(2,1)=6
-        Element(3,0)=7
-        Element(3,1)=8
-    */
+  
             
 
             //Abrir casa
